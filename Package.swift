22bf625f138a7a010fc2e1768defbc67069db6c2
yuhan <zhangyuhan0407@hotmail.com>
2017-03-12 15:35:28 +0800

    make public

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "OCT+Foundation",
+    name: "OCTFoundation",
     dependencies: [
         .Package(url: "https://github.com/zhangyuhan0407/OCTJSON", versions: Version(0,0,0)..<Version(10,0,0))
     ]
