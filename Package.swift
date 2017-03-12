54d5f15dde87c92d1fa41f8b9443a4bb8a4c17f4
yuhan <zhangyuhan0407@hotmail.com>
2017-03-12 15:56:38 +0800

    import dependency

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "OCTFoundation",
     dependencies: [
+        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 0, minor: 32),
         .Package(url: "https://github.com/zhangyuhan0407/OCTJSON", versions: Version(0,0,0)..<Version(10,0,0))
     ]
 )
