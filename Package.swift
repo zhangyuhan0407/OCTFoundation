0c632f205d2f5c9c153dbea65ed4a4977fb82585
yuhan <zhangyuhan0407@hotmail.com>
2017-05-17 12:34:51 +0800

    delete kitura

--- Package.swift
+++ Package.swift
@@ -3,7 +3,6 @@ import PackageDescription
 let package = Package(
     name: "OCTFoundation",
     dependencies: [
-//        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
         .Package(url: "https://github.com/zhangyuhan0407/OCTJSON", versions: Version(0,0,0)..<Version(10,0,0))
     ]
 )
