6c9b8086c52b104ad3db31e987feb778397df155
yuhan <zhangyuhan0407@hotmail.com>
2017-05-17 12:28:44 +0800

    v1.6.8

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "OCTFoundation",
     dependencies: [
-        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
+//        .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1, minor: 7),
         .Package(url: "https://github.com/zhangyuhan0407/OCTJSON", versions: Version(0,0,0)..<Version(10,0,0))
     ]
 )
--- Sources/OCT+RouterRequest.swift
+++ Sources/OCT+RouterRequest.swift
@@ -1,47 +1,47 @@
+////
+////  OCT+RouterRequest.swift
+////  OCTMemberServer
+////
+////  Created by yuhan zhang on 11/26/16.
+////
+////
 //
-//  OCT+RouterRequest.swift
-//  OCTMemberServer
+//import Foundation
+//import Kitura
+//import OCTJSON
 //
-//  Created by yuhan zhang on 11/26/16.
+//
+//public extension RouterResponse {
+//    
+//    public func send(_ s: CustomStringConvertible) -> RouterResponse {
+//        return self.send(s.description)
+//    }
+//    
+//}
 //
 //
-
-import Foundation
-import Kitura
-import OCTJSON
-
-
-public extension RouterResponse {
-    
-    public func send(_ s: CustomStringConvertible) -> RouterResponse {
-        return self.send(s.description)
-    }
-    
-}
-
-
-
-public extension RouterRequest {
-    
-    public var jsonBody: JSON? {
-        do {
-            
-            guard let s = try self.readString() else {
-                return nil
-            }
-            
-            return try JSON.deserialize(s)
-            
-        } catch {
-            return nil
-        }
-    }
-    
-}
-
-
-
-
-
-
-
+//
+//public extension RouterRequest {
+//    
+//    public var jsonBody: JSON? {
+//        do {
+//            
+//            guard let s = try self.readString() else {
+//                return nil
+//            }
+//            
+//            return try JSON.deserialize(s)
+//            
+//        } catch {
+//            return nil
+//        }
+//    }
+//    
+//}
+//
+//
+//
+//
+//
+//
+//
